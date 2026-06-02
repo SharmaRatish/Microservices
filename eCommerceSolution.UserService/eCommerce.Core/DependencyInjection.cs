979e3f1c8446441e2be71dc291be1f3f8b0a52e7
SharmaRatish <sratish746@gmail.com>
2026-06-02 22:57:31 +0530

    Addded Dapper

---
+++ eCommerceSolution.UserService/eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Core.ServiceContracts;
+using eCommerce.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core;
+
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Extension method to add core service to the dependency injection container
+        /// </summary>
+        /// <param name="IServiceCollection"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+        //To Do : Add service to the IoC container 
+        //Core service often include data access, caching and other low level components.
+        services.AddTransient<IUserService, UserService>();
+        return services;
+        }
+    }
+
+
